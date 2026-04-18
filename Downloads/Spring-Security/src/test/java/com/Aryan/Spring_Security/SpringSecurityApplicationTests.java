4a992d64063ed13edf33dccb38bb6d5d5eec5c80
Aryan <Aryankamboj2005@gmail.com>
2026-04-18 12:11:28 +0530

    architecture of the spring security

---
+++ Downloads/Spring-Security/src/test/java/com/Aryan/Spring_Security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aryan.Spring_Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
